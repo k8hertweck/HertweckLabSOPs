319a42aaf8d0c0f34446a33eeaba0708400c29a8
Kate Hertweck <k8hertweck@gmail.com>
2014-10-31 15:49:13 -0500

    Create sequenceAlign.sh
    Added sequence formats and general mafft parameters

---
+++ sequenceAlign.sh
@@ -0,0 +1,15 @@
+#/bin/bash
+
+###Multiple sequence alignment
+###File formats: 
+  ###XXX.fas is unaligned fasta format
+  ###XXX.afa is aligned fasta format
+  ###XXX.phy is aligned phylip format
+  
+###mafft http://mafft.cbrc.jp/alignment/software/ installed as mafft
+###general usage
+mafft XXX.fas > XXX.afa
+###align wiht G-INS-i (accurate method), sort sequences in output, output as phylip
+mafft --globalpair -maxiterate 16 --phylipout --reorder XXX.fas > XXX.phy
+###parameters for large trees http://mafft.cbrc.jp/alignment/software/tips.html
+###correcting for reverse complementation http://mafft.cbrc.jp/alignment/software/adjustdirection.html
